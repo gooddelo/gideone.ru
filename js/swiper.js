32d1dd0aa7a2e303b04f0bf8e03e83e3024aae33
Konstantin <Super.Juke88@yandex.ru>
2022-04-21 00:38:16 +0400

    Update 2022-04-20T20:38:09.322Z

--- js/swiper.js
+++ js/swiper.js
@@ -5,16 +5,17 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  loop: true,
   breakpoints: {
     // 320: {
     //   slidesPerView: 1,
     //   spaceBetween: 0,
     // },
-    // 768: {
-    //   slidesPerView: 2,
-    //   slidesPerGroup: 1,
-    //   spaceBetween: 72,
-    // },
+    768: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 30,
+    },
     1024: {
       slidesPerView: 2,
       slidesPerGroup: 1,
