87941ba4ced6de15284ebd7d88a0016cdd7f641e
Konstantin <Super.Juke88@yandex.ru>
2022-04-22 11:48:33 +0400

    Update 2022-04-22T07:48:31.009Z

--- js/animation.js
+++ js/animation.js
@@ -2,7 +2,7 @@ const wow = new WOW(
   {
     animateClass: 'animated', // animation css class (default is animated)
     offset:       50,          // distance to the element when triggering the animation (default is 0)
-    // mobile:       false,       // trigger animations on mobile devices (default is true)
+    mobile:       false,       // trigger animations on mobile devices (default is true)
     live:         true,       // act on asynchronously loaded content (default is true)
     scrollContainer: null,    // optional scroll container selector, otherwise use window,
     resetAnimation: true,     // reset animation on end (default is true)
