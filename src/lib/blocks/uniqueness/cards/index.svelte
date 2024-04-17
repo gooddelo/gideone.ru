d0213ff74dd87a053b6c0f720c0e48ed07ada96c
Pavel <tojefin@gmail.com>
2024-04-17 12:19:04 +0400

    ✨ feat: uniqueness desktop

---
+++ src/lib/blocks/uniqueness/cards/index.svelte
@@ -0,0 +1,71 @@
+<ul class="cards">
+	<li>
+		<img src="img/coin.svg" alt="рост среднего чека уже с 1го месяца" />
+		<p>рост среднего чека уже с 1го месяца</p>
+	</li>
+	<li class="opposite">
+		<p>рост числа довольных покупателей</p>
+		<img src="img/bord.svg" alt="рост числа довольных покупателей" />
+	</li>
+	<li>
+		<img src="img/rocket.svg" alt="снижение текучки кадров" />
+		<p>снижение текучки "кадров"</p>
+	</li>
+</ul>
+
+<style lang="scss">
+	ul {
+		display: flex;
+		justify-content: space-around;
+		margin-top: 30px;
+	}
+
+	li {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background: #212745;
+		padding: 40px 40px;
+		min-height: 438px;
+		aspect-ratio: 1/1;
+		&::after {
+			display: block;
+			content: '';
+			position: absolute;
+			background: url(img/star.svg) no-repeat;
+			background-size: contain;
+			width: 360px;
+			height: 360px;
+			z-index: 0;
+		}
+	}
+
+
+	.opposite {
+		img {
+			top: auto;
+			bottom: 20px;
+		}
+		p {
+			margin-top: 0;
+			margin-bottom: 240px;
+		}
+	}
+	img {
+		position: absolute;
+		top: -30px;
+		aspect-ratio: 1/1;
+		z-index: 2;
+	}
+	p {
+		margin-top: 130px;
+		font-weight: 500;
+		font-size: 30px;
+		text-transform: uppercase;
+		text-align: center;
+		color: #fff;
+		max-width: 330px;
+		z-index: 2;
+	}
+</style>
--- src/lib/blocks/uniqueness/index.svelte
+++ src/lib/blocks/uniqueness/index.svelte
@@ -1,20 +1,34 @@
+<script>
+	import Cards from './cards/index.svelte';
+</script>
+
 <article id="uniqueness">
 	<header>
 		<h2>ПОЧЕМУ МЫ?</h2>
 	</header>
+
+	<section class="mini">
+		<h4>Неоспоримые преимущества нашего ИИ-тренера</h4>
+	</section>
+
+	<Cards/>
+
 	<section>
 		<div>
 			<h3>СЦЕНАРИИ В GIDEONE</h3>
 			<p>
-				С нашей системой можно производить оценку и обучение не только по стандартам продаж,
-				например, но и по конкретным темам тренинга или внутренним сценариям продаж.
+				Сценарии в Gideone обеспечивают точную настройку обучения и оценки сотрудников для различных
+				продуктов и услуг, например, в банках, учебных центрах, и ритейле. Это улучшает развитие
+				навыков, адаптируя тренинги под конкретные задачи, что значительно повышает качество
+				обслуживания и увеличивает продажи.
 			</p>
 		</div>
 		<img src="img/scenario.svg" alt="оценка персонала по сценариям" />
 	</section>
+
 	<section class="operator">
 		<div>
-			<h3>У ВАС СВОЙ ОПЕРАТОР ВИДЕООЦЕНКИ?</h3>
+			<h3>У вас своя система контроля качества работы персонала?</h3>
 			<p>
 				У нас есть форматы для работы с компаниями, у которых есть свои операторы для оценки
 				персонала и работой с персоналом
@@ -22,20 +36,27 @@
 		</div>
 		<div class="features">
 			<div>
-				<img src="img/feedback.svg" alt="оценка персонала с обратной связью" />
-				<span>своевременную обратную связь</span>
+				<img src="img/feedback.svg" alt="персональные рекомендации для сотрудников in real time" />
+				<span>персональные рекомендации для сотрудников in real time</span>
 			</div>
 			<p>Мы обеспечим</p>
 			<div>
-				<img src="img/stats.svg" alt="контроль статистики и аналитики в оценке персонала" />
-				<span>контроль статистики и аналитики</span>
+				<img src="img/stats.svg" alt="управление качеством работы персонала" />
+				<span style="max-width: 380px;">управление качеством работы персонала</span>
 			</div>
 		</div>
 	</section>
+
 	<section>
 		<div>
-			<h3>КОМУ МЫ ПОЛЕЗНЫ</h3>
-			<p>Наша целевая аудитория - это и микро компании и крупные игроки</p>
+			<h3>Раскройте потенциал вашего бизнеса с Gideone</h3>
+			<p>
+				Независимо от того, управляете ли вы сетевым магазином, пекарней, фастфудом, фитнес-центром,
+				салоном красоты, медицинским учреждением или банком, Gideone улучшит ваше обслуживание и
+				продажи. Наша технология адаптирована для работы в прикассовых зонах, у прилавка и в зонах
+				рецепции, обеспечивая бизнесам любого масштаба необходимые инструменты для улучшения
+				качества обслуживания и увеличения продаж
+			</p>
 		</div>
 		<img src="img/store.svg" alt="оценка персонала для микро, малого и крупного бизнеса" />
 	</section>
@@ -51,50 +72,65 @@
 		margin-bottom: 30px;
 	}
 	h3 {
-		max-width: 763px;
+		max-width: 820px;
 		font-family: 'Unbounded', sans-serif;
+		text-transform: uppercase;
 		font-weight: 700;
-		font-size: 50px;
+		font-size: 40px;
 		color: #fff;
-		margin-bottom: 60px;
+		margin-bottom: 50px;
+	}
+	h4 {
+		font-weight: 700;
+		font-size: 30px;
+		color: #80f0ff;
 	}
 	p {
-		max-width: 763px;
+		max-width: 689px;
 		font-weight: 500;
-		font-size: 26px;
+		font-size: 23px;
 		color: #fff;
 	}
 
 	article {
-		margin-right: 160px;
 		display: flex;
 		flex-direction: column;
 		gap: 30px;
 	}
 
+	.mini {
+		padding: 45px 115px;
+		border-radius: 0 40px 40px 0;
+		max-width: 876px;
+		min-width: auto;
+		min-height: auto;
+	}
+
 	section {
+		margin-right: 160px;
 		padding: 75px 360px 75px 70px;
 		background: #212745;
 		border-radius: 0 90px 90px 0;
 		display: flex;
-		align-items: top;
+		align-items: center;
 		justify-content: space-between;
 		min-height: 404px;
 		max-width: 1440px;
 
 		img {
 			position: absolute;
-			right: 0;
-			top: 0;
+			right: 14px;
 		}
 	}
 
 	.operator {
 		display: flex;
 		flex-direction: column;
+		align-items: flex-start;
 		padding: 75px 0 75px 70px;
 
 		.features {
+			padding-right: 45px;
 			margin-top: 40px;
 			display: flex;
 			min-width: 100%;
@@ -113,11 +149,11 @@
 				justify-content: center;
 				align-items: flex-end;
 				min-width: 404px;
-				max-width: 450px;
+				max-width: 510px;
 				min-height: 404px;
 				span {
-					bottom: 60px;
-					max-width: 500px;
+					bottom: 40px;
+					max-width: 510px;
 					font-weight: 700;
 					font-size: 26px;
 					text-transform: uppercase;
